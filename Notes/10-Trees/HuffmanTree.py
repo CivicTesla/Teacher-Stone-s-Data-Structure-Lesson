b32195768468f539b1d3b174531963030544f29e
Jianwei Shi <jianwei.shi.cn@outlook.com>
2019-11-15 23:00:13 +0100

    fixed a bug, but the tree looks not the same as the example

--- Notes/10-Trees/HuffmanTree.py
+++ Notes/10-Trees/HuffmanTree.py
@@ -1,105 +1,57 @@
-import re
 class HuffmanNode(object):
-    def __init__(self, left=None, right=None, parent=None, char=None, visited=False):
+    def __init__(self, left=None, right=None):
         self.left = left
         self.right = right
-        self.parent = parent
-        self.char = char
-        self.visited = visited
-    def link_children(self, left=None, right=None):
-        self.left = left
-        self.right = right
-    def children(self):
-        return((self.left, self.right))
-    def toString(self):
-        print ("left node:", self.left)
-        print ("right node:", self.right)
 
 def create_tree(frequencies):
     while len(frequencies) > 1:           # 2. While there is more than one node
                                           # 2a. remove two highest nodes
         l, r = find_two_highest_feq(frequencies) 
-        combi_freq = l[0]+r[0]
-        left_leaf_char = l[1]
-        right_leaf_char = r[1]
-
+        combi_freq = l[0] + r[0]
                                           # 2b. create internal node with children
-        parent_node = HuffmanNode()
-        left_child = HuffmanNode(None, None, parent_node, left_leaf_char)
-        right_child = HuffmanNode(None, None, parent_node, right_leaf_char)
-        parent_node.link_children(left_child, right_child)
-    return parent_node                    # 3. tree is complete - return root node
+        parent_node = HuffmanNode(l, r)
+                                          # 2c. add new node to queue
+        combi_freq_item = (combi_freq, parent_node)
+        frequencies.append(combi_freq_item)
+    return frequencies[0]                 # 3. tree is complete - return root node
 
 # find two highest frequencies
 # and REMOVE THEM FROM the list
-# and combine them to a new freqency
+# and combine them to a new freqency (also combine the letters)
 def find_two_highest_feq(frequency_list):
     first_highest_feq = find_highest_feq(frequency_list)
     frequency_list.remove(first_highest_feq)
     second_highest_feq = find_highest_feq(frequency_list)
     frequency_list.remove(second_highest_feq)
     
-    frequency_list.append((first_highest_feq[0]+second_highest_feq[0], None))
-    
     return first_highest_feq, second_highest_feq
 
 def find_highest_feq(frequency_list):
     max_frequency = frequency_list[0]
     for frequency_item in frequency_list:
-        if frequency_item[0] > max_frequency[0]:
+        if float(frequency_item[0]) > float(max_frequency[0]):
             max_frequency = frequency_item
     return max_frequency
 
-# go through the tree
-# return a coding table
-def walk_tree(node, coding_stack, coding_table):
-    node_is_leaf = is_leaf(node)
-    if node_is_leaf or both_children_visited(node):
-        if node_is_leaf:
-            coding = stack2coding(coding_stack)
-            coding_table[coding]=node.char
-        node.visited = True
-        if len(coding_stack)==0:
-            return coding_table
-        else:
-            walk_tree(node.parent, coding_stack, coding_table)
-    elif node!=None: # PROBLEM HERE
-        # if the node is a node in the tree (not the bottom leaves, not the top root,
-        #                       not None either (a leave may have a None left and a None right))
-        if (not node.left.visited):
-            coding_stack.append(0)
-            walk_tree(node.left, coding_stack, coding_table)
-        if (not node.right.visited):
-            coding_stack.append(1)
-            walk_tree(node.right, coding_stack, coding_table)
-# PROMBLEM HERE: AttributeError: 'NoneType' object has no attribute 'left'
-# determine whether a node is a leaf
-def is_leaf(node):
-    if node == None:
-        return False
-    if node.left == None and node.right == None:
-        return True
-    return False
+# Recursively walk the tree down to the leaves,
+# assigning a code value to each symbol
+def walk_tree(node, prefix="", code={}):
+    node_or_leaf = node[1].left[1]
+    if isinstance(node_or_leaf, HuffmanNode):
+        left_node = node[1].left
+        walk_tree(left_node, prefix+"0", code)
+    else:
+        left_leaf = node_or_leaf
+        code[left_leaf]=prefix+"0"
 
-# determine if all children of a node are visited
-def both_children_visited(node):
-    left_visited = False
-    right_visited = False
-    if node.left !=None:
-        if node.left.visited:
-            left_visited = True
-    if node.right !=None:
-        if node.right.visited:
-            right_visited = True
-
-    return left_visited and right_visited
-
-# read the queue from left to right as coding
-def stack2coding(stack):
-    coding = ""
-    for item in stack:
-        coding+=str(item)
-    return coding
+    node_or_leaf = node[1].right[1]
+    if isinstance(node_or_leaf,HuffmanNode):
+        right_node = node[1].right
+        walk_tree(node[1].right,prefix+"1", code)
+    else:
+        right_leaf = node_or_leaf 
+        code[right_leaf]=prefix+"1"
+    return(code)
 
 if __name__ == '__main__':
     freq = [
@@ -113,6 +65,8 @@ if __name__ == '__main__':
 
     root_node = create_tree(freq)
     
-    coding_table = walk_tree(root_node, [], {})
-
-    print(coding_table)
+    code = walk_tree(root_node)
+    
+    for item in code:
+        print (item, code[item])
+    #print(coding_table)
